2822e89be0c2d7069cf0f4f1fd39be8745588131
Alex <alexanderbergquist80@gmail.com>
2022-03-15 15:25:47 +0100

    removed unused middleware

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const express = require('express');
 // instantiate express
 const app = express();
 
-
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-
 // serve static files from public folder
 app.use(express.static('public/static/'));
 
