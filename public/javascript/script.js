9976d6016909bc9506da8959b67648a4f0dae41c
Gustaf Grönlund <92786316+GustafGronlund@users.noreply.github.com>
2022-03-15 11:06:37 +0100

    Updated the virus so it moves on every click.

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,19 +2,22 @@
 
 let cursorImages = ["Handsprit.png", "Handsprit@2x", "Handsprit@3x"];
 let virus = document.querySelector('#virus');
-let randomGridNumberX = Math.floor(Math.random() * 11);
-let randomGridNumberY = Math.floor(Math.random() * 11);
 let rounds = 10;
 
-let randomGrid = {
-    gridColumnStart: randomGridNumberX,
-    gridColumnEnd: ++randomGridNumberX,
-    gridRowStart: randomGridNumberY,
-    gridRowEnd: ++randomGridNumberY
-};
-
-Object.assign(virus.style, randomGrid);
-
 virus.addEventListener('click', () => {
+    // Random numbers for grid
+    let randomGridNumberX = Math.floor(Math.random() * 11);
+    let randomGridNumberY = Math.floor(Math.random() * 11);
     console.log('hej')
+
+    // Adding the styling in a object so the virus can move around randon
+    let randomGrid = {
+        gridColumnStart: randomGridNumberX,
+        gridColumnEnd: ++randomGridNumberX,
+        gridRowStart: randomGridNumberY,
+        gridRowEnd: ++randomGridNumberY
+    };
+
+    // Assign the object to the virus so it moves around on every click
+    Object.assign(virus.style, randomGrid);
 })
