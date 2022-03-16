c4301e05bfaac9f737234f37da27203b465a2115
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-03-16 15:23:56 +0100

    Ändrat i CSS

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express');
-
+const express = require("express");
 
 // instantiate express
 const app = express();
 
 // serve static files from public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 module.exports = app;
--- bin/server.js
+++ bin/server.js
@@ -1,84 +1,78 @@
+/*
+ *   Module dependencies
+ */
+
+const app = require("../app");
+const http = require("http");
+const socketio = require("socket.io");
+const socket_controller = require("../controllers/socket_controller");
+const { debug } = require("console");
 
 /*
-*   Module dependencies
-*/
-
-const app = require('../app');
-const http = require('http');
-const socketio = require('socket.io');
-const socket_controller = require('../controllers/socket_controller');
-const { debug } = require('console');
+ *   Port
+ */
+const port = normalizePort("3000");
+app.set("port", port);
 
 /*
-*   Port
-*/
-const port = normalizePort('3000');
-app.set('port', port);
-
-/*
-*   Create HTTP server
-*/
-app.set('port', port);
+ *   Create HTTP server
+ */
+app.set("port", port);
 
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
-io.on('connection', socket => {
-    socket_controller(socket, io);
-})
+io.on("connection", (socket) => {
+  socket_controller(socket, io);
+});
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
+function normalizePort(val) {
+  const port = parseInt(val, 10);
 
-function normalizePort (val) {
-    const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    if (port >= 0) {
-        return port;
-    } 
-
-    return false;
+  return false;
 }
 
 /*
-*   Event listener for HTTP server "error"
-*/
-function onError (error) {
-    if(error.syscall !== 'listen') {
-        throw error;
-    }
+ *   Event listener for HTTP server "error"
+ */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-    const bind = typeof port === 'string' 
-        ? 'Pipe ' + port 
-        : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-        switch (error.code) {
-            case 'AccessError':
-                console.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'AddrInUseError':
-                console.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-};
+  switch (error.code) {
+    case "AccessError":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "AddrInUseError":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
 /*
-*   Event listener for HTTP server "listening" event
-*/
-function onListening () {
-    const addr = server.address();
-    const bind = typeof addr === 'string' 
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+ *   Event listener for HTTP server "listening" event
+ */
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
